911ed961b4bac54c642bca4e592d837a840c521e
marcdevi <85670906+marcdevi@users.noreply.github.com>
2021-09-23 12:32:42 +0200

    Add files via upload

---
+++ indexmast.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.burger')
+
+burger.addEventListener('click',() => {burger.classList.toggle('active')});
+
+
+function toggleMenu () {  
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', (e) => {    
+      navbar.classList.toggle('show-nav');
+    });    
+  }
+  toggleMenu();
